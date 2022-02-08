823b270d9b8da3934920131b8c8e07c4be89bc3e
Wilpola - PC <71035737+wilpola@users.noreply.github.com>
2022-02-08 11:02:10 +0200

    add: Constant colors

--- Flowga/constants.py
+++ Flowga/constants.py
@@ -8,3 +8,26 @@ screen = {
 # Colors
 white = (255, 255, 255)
 black = (0, 0, 0)
+
+# @Rasmus, 2/8/2022
+# tints are for highlighting the same color
+# shades are for, well, shades/shadows, for contrast
+red = (220, 53, 34)         #main red
+redtint = (224, 73, 55)     #for highlighting
+redshade = (132, 32, 20)    # a darker color for "shade"
+
+green = (88, 143, 39)
+greentint = (120, 195, 53)
+greenshade = (70, 114, 31)
+
+blue = (13, 71, 161)
+bluetint = (18, 98, 223)
+blueshade = (10, 57, 129)
+
+yellow = (255, 190, 0)
+yellowtint = (255, 203, 51)
+yellowshade = (230, 171, 0)
+
+gray = (217, 217, 217)
+graytint = (221, 221, 221)
+grayshade = (174, 174, 174)
