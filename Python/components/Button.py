bc214eef5036d13eb8d12136b3ef6ca19c5b8c4b
Ville Wilpola <71035737+wilpola@users.noreply.github.com>
2022-04-04 14:00:50 +0300

    Reformat the codebase (#9)
    * ignore local Flowga folder
    
    * init: main.py
    
    * Set up initial structure for the game
    
    * update: README.md
    
    * clean up
    
    * init: file for first launch
    
    * fix: settings.json file location
    
    * do not track __pycache__, or settings.json
    
    * init: Yoga pose file [ asana.json ]
    
    * add: screen fill color
    
    * fix: typo
    
    * add: logic for fullscreen flag
    
    * formatting
    
    * init: screens module
    
    * init: components module
    
    * init: screen component
    
    * add support for the Screen component
    
    * fix: logical issue --> data import location
    
    * remove __pycache__
    
    * add mainmenu buttons
    
    * init: Button component
    
    * change default launch to windowed
    
    The reason for this change is debugging
    
    * improve the settings retrieval process
    
    * add: depth var
    
    * fix: formatting
    
    * Draw items for each screen
    
    * feat: add hover check, and improve click event
    
    * fix: blurry text
    
    * reorient stored
    
    * add buttons for start sequence
    
    * add: lobby music
    
    * v1.3 works
    
    * Support secondary font
    
    * width + height + fix volume (0-1)
    
    * support caption and fill
    
    * untrack an unused file
    
    * add backButton to options Screen
    
    * add: bye() to mainMenuScreen
    
    * fix: init mixer module with volume = 0

---
+++ Python/components/Button.py
@@ -0,0 +1,102 @@
+# This is a simple bygame button that is reusable acress all of the screens
+# within the game.
+
+import pygame
+import sys
+pygame.init()
+
+class Button():
+    def __init__(self, loc, label="Label", x=100, y=100, w=100, h=35, bg="#333333", fntColor='#ffffff', radius = 5, font= 'primary'):
+        super().__init__
+        # print('Button was imported')
+        primaryFont = pygame.font.SysFont("Sans-serif", 22) 
+        largeFont = pygame.font.SysFont("Sans-serif", 32) 
+
+        # Set up local content
+        self.fntColor   = fntColor
+        self.radius     = radius
+        self.label      = label
+        self.font       = font
+        self.loc        = loc
+        self.bg         = bg
+        self.x          = x
+        self.y          = y
+        self.w          = w
+        self.y          = y
+
+        # This is a local copy of all the elements, if they were to change during 
+        # the lifecycle of a given button component
+        self.stored = { 
+            "buttonColor":  f"{bg}", 
+            "fntColor":     f"{fntColor}", 
+            "label":        f"{label}",
+            "loc":          f"{loc}", 
+            "bg":           f"{bg}", 
+            "x":            f"{x}", 
+            "y":            f"{y}", 
+            "w":            f"{w}", 
+        }
+
+        # "": f"{}",
+
+        # Set up button "Rectangle"
+        self.buttonSurface  = pygame.Rect(x, y, w, h)
+        self.buttonColor    = self.bg
+        self.pressed        = False            # This is to handle click events
+
+        # Set up "Label"
+        if self.font == 'primary':
+            self.label      = primaryFont.render(self.label, True, self.fntColor)
+        elif self.font == 'L':
+            self.label       = largeFont.render(self.label, True, self.fntColor)
+        self.textArea   = self.label.get_rect(center = self.buttonSurface.center)
+        # self.cursor     = pygame.image.load('Python/assets/target.png').convert()
+        # self.curRect    = self.cursor.get_rect()
+        
+    
+    # Return [ True / False] whether hovering on the button
+    def isHovered(self):
+
+         # Get mouse position
+        mouse = pygame.mouse.get_pos()
+        
+        # Check if mouse is on top of the button
+        if self.buttonSurface.collidepoint(mouse):  
+            return True
+        else:
+            return False
+
+    # Action: What should happen when hovered?
+    def buttonHover(self):
+        # 
+        if self.isHovered():
+            self.buttonColor = (240, 0, 100)
+        else:
+            self.buttonColor = self.stored['buttonColor']
+
+    def handleClick(self):
+        # Get mouse position
+        # mouse = pygame.mouse.get_pos()
+
+        if self.isHovered():
+            # print('hovering')
+            self.buttonHover()
+            if pygame.mouse.get_pressed()[0]:
+                self.pressed = True
+            else:
+                if self.pressed == True:
+                    self.pressed = False
+    
+        else:
+            self.buttonHover()
+
+    # Draw the Button to the screen
+    def DrawButton(self):
+        pygame.draw.rect(self.loc, self.buttonColor, self.buttonSurface, border_radius = self.radius)
+        self.loc.blit(self.label, self.textArea)
+        
+        # Click Events 
+        # TODO: This may get deleted, as it may not be necessary
+        self.handleClick()
+        # self.buttonHover()
+        pygame.display.update()
---
+++ Python/components/Screen.py
@@ -0,0 +1,71 @@
+import json, sys, pygame
+# import pygame_menu
+
+class Screen():
+
+    # Initialize constructor
+    def __init__(self, caption="Screen", bg="#ffffff"):
+
+        # Get display data from settings
+        with open("Python/settings.json", "r") as j:
+            self.data = json.loads(j.read())
+        j.close()
+
+        # Set up local variables
+        self.fullscreen = self.data['fullscreen']
+        self.caption    = caption
+        self.Active     = False
+        self.bg         = bg
+
+    # Function to Activate the screen
+    def activateScreen(self):
+        # Set the window Active
+        self.Active = True
+
+        # Set window caption
+        pygame.display.set_caption(self.caption)
+
+        # -- Check whether the window is supposed to be fullscreen --#
+        # If fullscreen
+        if self.fullscreen  == True:
+            self.screen = pygame.display.set_mode((self.data["width"], self.data["height"]), pygame.FULLSCREEN, 32)
+
+        # If not fullscreen
+        if self.fullscreen  == False:
+            self.screen = pygame.display.set_mode((self.data["width"], self.data["height"]), depth=32)
+
+        # Set screen background
+        self.screen.fill(self.bg)
+
+    def setCaption(self):
+        pygame.display.set_caption(self.caption)
+
+    # Function to Deactivate the screen
+    def deactivateScreen(self):
+        # Set window Unactive
+        self.Active = False
+
+    # Check whether the Window is Active or not
+    # --> Return [ True / False]
+    def isActive(self):
+        return self.Active
+    
+    # Run update sequence
+    def updateScreen(self):
+        # If the screen is Active
+        if self.isActive():
+            self.activateScreen()
+        # If screen is not Active --> Hide it
+        else:
+            self.deactivateScreen()
+
+        # Actually update the screen
+        pygame.display.update()
+
+    
+    # Blitting function, to place buttons and other items on to the screen
+    def blit(self, surf, loc):
+        self.screen.blit(surf, loc)
+
+    def fill(self):
+        self.screen.fill(self.bg)
